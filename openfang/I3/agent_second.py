eafe909270e3d75e7ac221f998217c578459cbd5
KhanhCuong2510 <127456959+KhanhCuong2510@users.noreply.github.com>
2026-03-12 16:34:38 +0700

    Update and rename Reviewer to agent_second.py

---
+++ openfang/I3/agent_second.py
@@ -0,0 +1,20 @@
+# agent_second.py
+
+def tool_summarize(text):
+    """Tool tóm tắt đơn giản"""
+    summary = str(text).split(".")[0]
+    return summary
+
+class Reviewer:
+    def __init__(self):
+        self.name = "Reviewer"
+        self.description = "Receive processed text and summarize."
+
+    def run(self, text):
+        print("\n[Reviewer dang danh gia ket qua...]")
+
+        summary = tool_summarize(text)
+
+        print("Summary:", summary)
+
+        return summary
