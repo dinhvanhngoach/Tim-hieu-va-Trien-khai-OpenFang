607a8d935ad0a2c59fa10b2e0bc553e552825fc5
KhanhCuong2510 <127456959+KhanhCuong2510@users.noreply.github.com>
2026-03-12 16:21:01 +0700

    Update main.py

--- openfang/I3/main.py
+++ openfang/I3/main.py
@@ -1,13 +1,15 @@
 #agent 1 xử lý input và truyền kết quả sang Reviewer
 #import Reviewer vào agent_basic.py và gọi nó sau khi Agent 1 xử lý xong
-# agent_second.py
+# agent_second.py ( I1 ) 
+# agent_basic.py ( I3 )
+
 # agent_basic.py
+
 from agent_second import Reviewer
 
-# --- TOOL ---
+# --- TOOLS ---
 def tool_uppercase(text):
     return text.upper()
-
 def tool_double(number):
     return int(number) * 2
 
@@ -17,42 +19,35 @@ class Agent:
         self.name = name
         self.description = description
         self.tools = tools_list
-
     def run(self, tool_name, input_data):
-
-        print(f"\n[Agent {self.name} dang hoat dong...]")
-
+        print(f"\n[Agent {self.name} dang xu ly input...]")
         if tool_name in self.tools:
-
             result = self.tools[tool_name](input_data)
-
-            print(f"Su dung cong cu: {tool_name}")
-            print(f"Ket qua: {result}")
-
+            print("Tool duoc su dung:", tool_name)
+            print("Ket qua sau khi xu ly:", result)
             return result
-
         else:
             print("Khong tim thay tool")
             return None
 
-# --- MAIN ---
+# --- MAIN PIPELINE ---
 if __name__ == "__main__":
-
-    danh_sach_tools = {
+    tools = {
         "uppercase": tool_uppercase,
         "double": tool_double
     }
     agent1 = Agent(
-        name="TextProcessor",
+        name="Processor",
         description="Xu ly input bang tools",
-        tools_list=danh_sach_tools
+        tools_list=tools
     )
     reviewer = Reviewer()
-    tool = input("Nhap tool (uppercase / double): ")
+    # bước 1: user input
+    tool = input("Nhap tool (uppercase/double): ")
     user_input = input("Nhap du lieu: ")
 
-    # Agent 1 xử lý
-    result = agent1.run(tool, user_input)
+    # bước 2: Agent1 xử lý
+    processed_result = agent1.run(tool, user_input)
 
-    # Agent 2 nhận kết quả
-    reviewer.run(str(result))
+    # bước 3: Reviewer nhận kết quả
+    reviewer.run(processed_result)
